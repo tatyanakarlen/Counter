9f982b7d64ccb95a5cf7f2bb8202d9f1b5eb8228
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-04-10 12:15:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+
+const plusBtn = document.getElementById("plus")
+plusBtn.addEventListener('click', calculate)
+
+const minusBtn = document.getElementById("minus")
+minusBtn.addEventListener('click', calculate)
+
+const input = document.getElementById("input")
+
+let H1 = document.getElementById("H1")
+
+let displayedNumber = parseInt(H1.innerHTML)
+
+let sum = 0
+
+let parsedInput = parseInt(input.value)
+
+
+
+
+
+
+function render() {
+    if (sum < 0) {
+        H1.innerHTML.style.color = 'red'
+    }
+}
+
+function calculate(e) {
+    if (e.target.id === "plus") {
+        sum += parseInt(input.value) + displayedNumber
+        H1.innerHTML = sum
+        render()
+    } else if (e.target.id === "minus") {
+        sum -= parseInt(input.value) - displayedNumber
+        H1.innerHTML = sum
+        render()
+    }
+}
+
+        // render function for some reason doesn't work, I am out
+    //   of energy 
+
+
+
+
+
+
+
+
+
+
